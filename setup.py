bfef6b9e6009034d2db82d9b82668eaa12794221
slarinier <slarinier@sekoia.fr>
2015-01-26 17:59:55 +0100

    add setup.py to build pe-tools

---
+++ setup.py
@@ -0,0 +1,15 @@
+'''
+Created on 26 janv. 2015
+
+@author: slarinier
+'''
+from distutils.core import setup
+
+setup(
+    name='pe-tools',
+    author='Sekoia',
+    author_email='cert@sekoia.fr',
+    version='0.1',
+    license='GPL v3',
+    packages=['petools',]
+)
